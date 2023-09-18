34969e61adb953ae0ce3b3d85eca9ef09722e396
Dhaval Bhatia <dhavalbhatia@Dhavals-MacBook-Pro.local>
2023-09-18 16:55:31 -0600

    fixed url

--- print_requests.py
+++ print_requests.py
@@ -1,6 +1,6 @@
 import requests
 
-URL = "https://github.com/dhavalbhatia/cmput404-lab1/blob/main/print_requests.py"
+URL = "https://raw.githubusercontent.com/dhavalbhatia/cmput404-lab1/main/print_requests.py"
 
 request = requests.get(URL)
 print(request.text)
